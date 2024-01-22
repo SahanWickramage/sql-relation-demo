d04efac2887438eca6ac1e818322d5a4e941f9c6
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-01-22 21:21:29 +0530

    initial project setup

---
+++ src/main/java/io/github/sahawick/sqlrelationdemo/SqlRelationDemoApplication.java
@@ -0,0 +1,13 @@
+package io.github.sahawick.sqlrelationdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SqlRelationDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SqlRelationDemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/github/sahawick/sqlrelationdemo/SqlRelationDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.sahawick.sqlrelationdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SqlRelationDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
