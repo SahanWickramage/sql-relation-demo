24a64a46b97ac5646f101f43774bfe5e02f4d4ee
Sahan Wickramage <35292360+SahanWickramage@users.noreply.github.com>
2024-03-02 16:52:36 +0530

    adding category cart

---
+++ src/main/java/io/github/sahawick/sqlrelationdemo/entity/Cart.java
@@ -0,0 +1,23 @@
+package io.github.sahawick.sqlrelationdemo.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Cart {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    @OneToOne(cascade = CascadeType.ALL)
+    @JoinColumn(name = "cart_id")
+    private Order order;
+}
